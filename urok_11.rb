5e5ccad79faed4f75fa4ec023470b84375b0ed81
razhardbass <mixan_12@mail.ru>
2019-09-30 00:27:28 +0300

    init

---
+++ urok_11.rb
@@ -0,0 +1,10 @@
+puts "Как вас зовут?"
+name = gets.chomp
+puts "Какой твой рост?"
+height = gets.chomp
+difference = height.to_i-110
+if difference > 0
+  puts "Привет, #{name}! Твой идеальный вес #{height.to_i-110}"
+ else
+   puts "Братишка #{name} , твой вес идеален!"
+ end
