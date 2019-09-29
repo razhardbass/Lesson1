3264869f1c218a5378cd2a14332342b81172fafa
razhardbass <mixan_12@mail.ru>
2019-09-30 00:33:47 +0300

    refactored

--- urok_11.rb
+++ urok_11.rb
@@ -3,6 +3,7 @@ name = gets.chomp
 puts "Какой твой рост?"
 height = gets.chomp
 difference = height.to_i-110
+
 if difference > 0
   puts "Привет, #{name}! Твой идеальный вес #{height.to_i-110}"
  else
