2d87d1a33f71cffadf4b66fbd70b148a867648c2
Misha <mian_12@mail.ru>
2020-05-13 18:19:30 +0300

    style edits

--- lesson1/ideal_weight.rb
+++ lesson1/ideal_weight.rb
@@ -1,10 +1,10 @@
-puts "Как тебя зовут?"
+puts 'Как тебя зовут?'
 name = gets.chomp
-puts "Какой твой рост?"
+puts 'Какой твой рост?'
 height = gets.chomp
-idealw = (height.to_i - 110)*1.15
+idealw = (height.to_i - 110) * 1.15
 if idealw < 0
-    puts "Ваш вес уже оптимальный"
-else 
-    puts "#{name}, твой идеальный вес #{idealw}"
+  puts 'Ваш вес уже оптимальный'
+else
+  puts "#{name}, твой идеальный вес #{idealw}"
 end
