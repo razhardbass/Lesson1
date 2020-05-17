76d33bc0faa3d86db49eef347d5bd9da86f7d859
Misha <mixan_12@mail.ru>
2020-05-17 17:17:33 +0300

    correction leap_date.rb

--- lesson2/leap_date.rb
+++ lesson2/leap_date.rb
@@ -2,10 +2,22 @@
 
 puts 'Введите число месяца'
 number = gets.to_i
+until number.between?(1,31)
+  puts 'Введите число месяца от 1 до 31'
+  number = gets.to_i
+end
 puts 'Введите номер месяца'
 month = gets.to_i
+until month.between?(1,12)
+  puts 'Введите номер месяца от 1 до 12'
+  month = gets.to_i
+end
 puts 'Введите год'
 year = gets.to_i
+until year > 0
+  puts 'Введите положительное значение'
+  year = gets.to_i
+end
 month_array = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 month_array[1] = if year % 4 == 0
                    29
